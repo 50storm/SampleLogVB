5a8480bc0af8e3833332937894842b2b64887893
五十嵐博司 <aaa@test.com>
2016-05-27 17:46:46 +0900

    first commit

---
+++ Module1.vb
@@ -0,0 +1,21 @@
+﻿
+Module Module1
+
+    Sub Main()
+        Try
+            Dim File As String = My.Settings("ErrorLog").ToString()
+            Using Writer As New IO.StreamWriter(File, True, Text.Encoding.UTF8)
+                Writer.WriteLine("ChkShtError" & DateTime.Now.ToString())
+                Writer.WriteLine()
+                Writer.Close()
+            End Using
+
+
+        Catch ex As Exception
+
+        End Try
+
+
+    End Sub
+
+End Module
